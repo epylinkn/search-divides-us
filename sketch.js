1c7fea03e567dd676ae416df0989a06be0128e79
Kathy Wu <kwu01@risd.edu>
2018-04-01 19:48:38 -0400

    scene manager with three scenes

--- sketch.js
+++ sketch.js
@@ -1,7 +1,90 @@
-function setup() {
-  createCanvas(400, 400);
+var mgr;
+
+function setup()
+{
+    createCanvas(600, 500);
+
+    mgr = new SceneManager();
+
+    // Preload scenes. Preloading is normally optional
+    // ... but needed if showNextScene() is used.
+    mgr.addScene ( Intro );
+    mgr.addScene ( Game );
+    mgr.addScene ( Outro );
+
+    // mgr.showNextScene();
+    mgr.showScene( Intro );
+
 }
 
-function draw() {
-  background(220);
+function draw()
+{
+    mgr.draw();
+}
+
+function mousePressed()
+{
+    mgr.mousePressed();
+}
+
+function keyPressed()
+{
+    // You can optionaly handle the key press at global level...
+    switch(key)
+    {
+        case '1':
+        print("1 is pressed");
+            mgr.showScene( Intro );
+            break;
+        case '2':
+        print("2 is pressed");
+
+            mgr.showScene( Game );
+            break;
+        case '3':
+        print("3 is pressed");
+
+            mgr.showScene( Outro );
+            break;
+    }
+
+    // ... then dispatch via the SceneManager.
+    mgr.keyPressed();
+}
+
+
+function Intro(){
+    this.setup = function(){
+    }
+
+    this.draw = function() {
+      background(255);
+      fill(0);
+      text("welcome to optimization city. intro text!", width/2, height/2);
+    }
+}
+
+
+function Game(){
+  this.setup = function(){
+  }
+
+  this.draw = function() {
+    background(0);
+    fill(255);
+    text("MAP GOES HERE", width/2, height/2)
+  }
+
+}
+
+function Outro(){
+    this.setup = function(){
+    }
+
+    this.draw = function() {
+      background(255);
+      fill(0);
+
+      text("forcefeeding you the message of the game", width/2, height/2);
+    }
 }

22bce0aa7c36b989317ede3fe8e326d08e2a10ab
Anthony Bui <anthonybui@gmail.com>
2018-04-29 21:51:29 -0400

    :speedboat: Stop doing so much html in the draw lopp

--- public/assets/js/Profile.js
+++ public/assets/js/Profile.js
@@ -1,30 +1,30 @@
 class Profile extends GenericScene {
+  setup() {
+    this.incomeLabel = select("#inc-label");
+    this.raceLabel = select("#race-label");
+    this.educationLabel = select("#edu-label");
+  }
+
   enter() {
     super.enter()
     select(".foo").show();
+
+    this.drawOnce();
   }
 
   leave() {
     select(".foo").hide();
   }
 
-  draw() {
+  drawOnce() {
+    push();
+
     background(browser,1);
 
-    push();
+    imageMode(CENTER);
     fill(255);
-
-    // textSize(30);
-    // text(
-    //   squish(`YOUR PROFILE`),
-    //   width/2,
-    //   height/4 - 100
-    //
-    // );
-
     textSize(16);
     textLeading(28);
-
     text(
       squish(`Tell us about yourself so we can give you
       the best possible search experience.`),
@@ -32,19 +32,17 @@ class Profile extends GenericScene {
       height/4 - 50
     );
 
-    // TODO: animate profile as selections change
-    imageMode(CENTER)
-    image(you, width/2,height/2-70,220,220);
-
     tint(255, 255);  // Display at FULL opacity
     image(button_search, width/2,height-100,285,65)
+
     pop();
+  }
 
-    select("#inc-label").html(placeholder(incomeLabels[selections.income]));
-    select("#race-label").html(placeholder(raceLabels[selections.race]));
-    select("#edu-label").html(placeholder(educationLabels[selections.education]));
-
-    // displayInstruction("TURN KNOBS AND THEN PRESS ENTER TO FIND A NEIGHBORHOOD");
+  draw() {
+    push()
+    imageMode(CENTER)
+    image(you, width/2,height/2-70,220,220);
+    pop()
   }
 
   canSeeNextScene() {
@@ -62,49 +60,63 @@ class Profile extends GenericScene {
       //== Income
       case 'Q':
         selections.income = 0;
+        this.incomeLabel.html(incomeLabels[selections.income]);
         break;
       case 'W':
         selections.income = 1;
+        this.incomeLabel.html(incomeLabels[selections.income]);
         break;
       case 'E':
         selections.income = 2;
+        this.incomeLabel.html(incomeLabels[selections.income]);
         break;
       case 'R':
         selections.income = 3;
+        this.incomeLabel.html(incomeLabels[selections.income]);
         break;
 
       //== Race
       case 'A':
         selections.race = 0;
+        this.raceLabel.html(raceLabels[selections.race]);
         break;
       case 'S':
         selections.race = 1;
+        this.raceLabel.html(raceLabels[selections.race]);
         break;
       case 'D':
         selections.race = 2;
+        this.raceLabel.html(raceLabels[selections.race]);
         break;
       case 'F':
         selections.race = 3;
+        this.raceLabel.html(raceLabels[selections.race]);
         break;
       case 'G':
         selections.race = 4;
+        this.raceLabel.html(raceLabels[selections.race]);
         break;
 
       //== Education
       case 'Z':
         selections.education = 0;
+        this.educationLabel.html(educationLabels[selections.education]);
         break;
       case 'X':
         selections.education = 1;
+        this.educationLabel.html(educationLabels[selections.education]);
         break;
       case 'C':
         selections.education = 2;
+        this.educationLabel.html(educationLabels[selections.education]);
         break;
       case 'V':
         selections.education = 3;
+        this.educationLabel.html(educationLabels[selections.education]);
         break;
       case 'B':
         selections.education = 4;
+        this.educationLabel.html(educationLabels[selections.education]);
         break;
 
       case 'I':

77ba10019115ff4aa9546fcf7b25be9b9372e8b5
Anthony Bui <anthonybui@gmail.com>
2018-04-29 21:38:06 -0400

    Intro scene upgrades!
    * Fade in the signup screen
    * Only react to search button

--- public/assets/js/Intro.js
+++ public/assets/js/Intro.js
@@ -1,56 +1,40 @@
 class Intro extends GenericScene {
   setup() {
     this.fadeTime = 2*1000;
+
+    this.drawOnce();
   }
 
   enter() {
     super.enter()
-    this.loadingEndAt = millis() + this.fadeTime;
+    this.enteredAt = millis();
+    this.fadeInAt = this.enteredAt + 3*1000;
+  }
+
+  drawOnce() {
+    background(browser,1);
   }
 
   draw() {
-    background(browser,1);
     image(mapTeaser,0,0,width, height);
 
     let currentMillis = millis();
-        let diff = constrain(this.loadingEndAt - currentMillis, 0, this.fadeTime)
-        let alpha = map(diff, 0, this.fadeTime, 255, 0)
-        console.log(alpha)
 
-        push();
-          tint(255,alpha)
-          image(welcome, 0,0, width,height)
-        pop();
+    if (currentMillis > this.fadeInAt) {
+      let diff = constrain(currentMillis - this.fadeInAt, 0, this.fadeTime)
+      let alpha = map(diff, 0, this.fadeTime, 0, 255)
 
+      push();
+      tint(255,alpha)
+      image(welcome, 0,0, width,height)
+      pop();
+    }
   }
 
   keyPressed() {
     switch(key) {
-      //== Income
-      case 'Q':
-      case 'W':
-      case 'E':
-      case 'R':
-
-      //== Race
-      case 'A':
-      case 'S':
-      case 'D':
-      case 'F':
-      case 'G':
-
-      //== Education
-      case 'Z':
-      case 'X':
-      case 'C':
-      case 'V':
-      case 'B':
-
-      case 'I':
       case 'O':
         buttonSound.play();
-      case 'P':
-      case 'CATCH':
         this.sceneManager.showNextScene();
         break;
     }
